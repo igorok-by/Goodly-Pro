18ae449b23759e40cfae9491c7bd27052d63d3fe
Igor <igorko.by@gmail.com>
2019-12-12 07:42:26 +0300

    Finish Tiles, Subscribe Form, Footer; create /dist

--- src/js/index.js
+++ src/js/index.js
@@ -2,58 +2,19 @@
 
 $(function () {
 
-  // LET IT SCROLL!
-  let btn = $('#scrollTop');
+  let tile = $('.tile'),
+      popup = $('.tile__popup'),
+      openPopup = $('.tile__popup-open-js'),
+      closePopup = $('.tile__popup-close'),
+      classToggled = 'tile__popup--shown',
+      
+      popupToggle = function () {
+        $(this)
+          .closest(tile)
+          .find(popup)
+          .toggleClass(classToggled);
+      };
 
-  $(window).scroll(function() {
-    $(window).scrollTop() > 300 ? btn.addClass('show') : btn.removeClass('show');
-  });
-  
-  $('a[href^="#"]').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({
-      scrollTop: $( $(this).attr('href')).offset().top
-    }, 700 );
-  });
-
-  if ($(window.location.hash).length > 1) {
-    $('html, body').animate({
-      scrollTop: $(window.location.hash).offset().top
-    }, 700);
-  };
-
-  //  CONTACT FORM
-  let formContact = $("form[name=CONTACT_FORM]"),
-      inputName = $('input[name=contact_name]'),
-      inputEmail = $('input[name=contact_email]'),
-      popupSuccess = $('.form__popup--success'),
-      popupError = $('.form__popup--error'),
-      mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      errorClass = 'form__wrap-input--error';
-
-  formContact.submit(function (event) {
-    event.preventDefault();
-
-    inputName.val() ? inputName.parent().removeClass(errorClass) : inputName.focus().parent().addClass(errorClass);
-
-    inputEmail.val().match(mailformat) ? inputEmail.parent().removeClass(errorClass) : inputEmail.focus().parent().addClass(errorClass);
-
-    var formData = {
-      'name': inputName.val(),
-      'email': inputEmail.val()
-    };
-
-    if (inputEmail.val() && inputName.val()) {
-      $.ajax({
-        type: 'POST',
-        url: 'myServerURL.php', // the url where we want to POST
-        data: formData,
-        dataType: 'json'
-      })
-
-      .done(function (data) {
-        data.success ? popupSuccess.addClass('.form__popup--show') : popupError.addClass('.form__popup--show');
-      });
-    }
-  });
+  openPopup.on('click', popupToggle);
+  closePopup.on('click', popupToggle);
 });
